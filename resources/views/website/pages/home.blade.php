58f675af66851769dcaa3b63adfd1c7109468fc4
tony <antonio.comique@ruralnet.ph>
2023-12-06 10:34:27 +0800

    update home.blade.php - clean comments.

--- resources/views/website/pages/home.blade.php
+++ resources/views/website/pages/home.blade.php
@@ -257,25 +257,6 @@
 <!----------------------------- end of home_faq ---------------------> 
 
 
-{{-- <div class="home_banner3">
-    <div class="container">
-        <div id="hb3_left">
-            <div>
-                <p>
-                    Contact us today if you have any questions about our 
-                    programs, your CashKO policy, or partnership opportunities.
-                </p>
-                <img src="{{ asset('images/CashKO_Accent-5.png') }}" />
-                <img src="{{ asset('images/ruralnet-solutions-icon-4.png') }}" />
-            </div>
-        </div>
-        <div id="hb3_right">
-            <div>
-                <button class="btn_big">SEND US A MESSAGE</button>
-            </div>    
-        </div>
-    </div>
-</div> --}}
 <x-banner-send-message></x-banner-send-message>
 <!----------------------------- end of home_faq ---------------------> 
 @endsection
